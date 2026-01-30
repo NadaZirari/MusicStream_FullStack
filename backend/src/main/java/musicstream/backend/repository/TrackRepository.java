7d1af3121660b58361031fadb9615f393779c1e3
Nada Zirari <zirarinada20@gmail.com>
2026-01-30 12:21:32 +0100

    ajouter repository concernatnt le track

---
+++ backend/src/main/java/musicstream/backend/repository/TrackRepository.java
@@ -0,0 +1,9 @@
+package musicstream.backend.repository;
+
+import musicstream.backend.model.Track;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TrackRepository extends JpaRepository<Track, Long> {
+}
