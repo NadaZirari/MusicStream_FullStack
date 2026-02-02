009ce1724640a4c5beebe455e68f2ee87ce90553
Nada Zirari <zirarinada20@gmail.com>
2026-02-02 16:23:52 +0100

    ajouter dans controller endpoint pour modifier

---
+++ backend/src/main/java/musicstream/backend/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package musicstream.backend.exception;
+
+public class GlobalExceptionHandler extends RuntimeException {
+  public GlobalExceptionHandler(String message) {
+    super(message);
+  }
+}
